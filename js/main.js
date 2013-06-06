18d53351b71a23141c331d9e3c4f68a6be080a09
Oscar Paucara <oscar.paucara@globant.com>
2013-06-06 12:43:18 -0300

    require-js and amd vendor libs updated

---
+++ js/main.js
@@ -0,0 +1,11 @@
+require.config({
+  paths: {
+    jquery: 'libs/jquery-1.9.1',
+    underscore: 'libs/underscore',
+    backbone: 'libs/backbone'
+  }
+});
+
+require(['router'], function(App) {
+
+});
