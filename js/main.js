6ccc53d0854c4c477691dbccd9bafd617b8483d6
Oscar Paucara <oscar.paucara@globant.com>
2013-06-06 17:31:36 -0300

    Adding handlebars amd

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ require.config({
   paths: {
     jquery: 'libs/jquery-1.9.1',
     underscore: 'libs/underscore',
-    backbone: 'libs/backbone'
+    backbone: 'libs/backbone',
+    handlebars: 'libs/handlebars'
   }
 });
 
