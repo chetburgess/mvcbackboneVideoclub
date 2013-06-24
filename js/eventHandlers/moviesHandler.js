f2c796c314e5d1ca40c5093a5557f37349d348a0
Oscar Paucara <oscar.paucara@globant.com>
2013-06-24 16:41:27 -0300

    Adding eventHandlers obj

---
+++ js/eventHandlers/moviesHandler.js
@@ -0,0 +1,8 @@
+define([
+  'backbone'
+],
+function () {
+
+  var MoviesHandler = _.extend({}, Backbone.Events);
+  return MoviesHandler;
+});
---
+++ js/eventHandlers/usersHandler.js
@@ -0,0 +1,8 @@
+define([
+  'backbone'
+],
+function () {
+
+  var UsersHandler = _.extend({}, Backbone.Events);
+  return UsersHandler;
+});
